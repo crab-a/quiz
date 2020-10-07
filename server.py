933c49693921b7f63b82b7a088764180b5404ae1
crab-a <crab@notmailinator.com>
2020-10-07 08:47:20 +0300

    start the project

---
+++ server.py
@@ -0,0 +1,90 @@
+import markdown
+import os
+import csv
+
+from flask import Flask, render_template, request, redirect
+
+# def md_to_html(md_text):
+#     md_text = md_text.read()
+#     html = markdown.markdown(md_text)
+#     return html
+# for md_txt_filename in os.listdir(r'.\static\assets\texts'):
+#     if md_txt_filename.endswith(".md"):
+#         with open(f'.\\static\\assets\\texts\\{md_txt_filename}', 'r', encoding='utf-8') as md_file:
+#             html_text = md_to_html(md_file)
+#             html_txt_filename = md_txt_filename.replace('.md', '.html')
+#         with open(f'.\\static\\assets\\texts\\{html_txt_filename}', 'w', encoding='utf-8') as html_file:
+#             html_file.write(html_text)
+quiz = Flask(__name__)
+page_num = 1
+right_answers=['0=so empty', 'בית החולים כפר שאול', 'next answer', 'etc']
+@quiz.route('/')
+def my_home():
+    return render_template('Q1.html')  # change to intro.html
+
+
+@quiz.route('/<string:current_page>')
+def page_name(current_page=''):
+    global page_num
+    try:
+        page_num = int(current_page[1])
+    except:
+        pass
+    return render_template(current_page)
+
+
+@quiz.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        global page_num
+        try:
+            answer = request.form.get('answer')
+            answers.append(answer)
+            if answer == right_answers[page_num]:
+                return redirect('right_answer.html')
+            else:
+                return redirect('wrong_answer.html')
+        except:
+            return 'couldnt get that try again'
+    else:
+        return redirect(f'A{page_num}.html')
+
+@quiz.route('/try_again')
+def try_again():
+    return render_template(f'Q{page_num}.html')
+
+@quiz.route('/full_answer')
+def full_answer():
+    return render_template(f'A{page_num}.html')
+
+
+@quiz.route('/nextq')
+def nextq():
+    global page_num
+    page_number = request.args.get('page_number')
+    return render_template(f'Q{page_num+1}.html')
+
+
+@quiz.route('/back')
+def back():
+    global page_num
+    return render_template(f'Q{page_num-1}.html')
+
+
+def write_to_csv(answer, username):
+    with open('database.csv', newline='', mode='a') as database:
+        csv_writer = csv.writer(database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([answer])
+
+
+answers = []
+
+# implment all q&a in the html templet of q and a
+# in the htmls change all <p> to <p dir="rtl"> to make rtl
+# get a username
+# export all answers to a csv/google sheets
+# move nav bar to middle
+# get right answers by order into the list and accsess by page num
+    # you can use dict with numbers as keys to get multiple possible answers
+
+#treat edge cases (first qustion back and last qustion next)
---
+++ static/assets/texts/colophone.md
@@ -0,0 +1,8 @@
+
+<p dir="rtl">
+"כל המתאבל על ירושלים זוכה ורואה בשמחתה"</p>
+
+
+<p dir="rtl">
+אם נהנתם מהחידון אתם יכולים לנסות את <a href="https://docs.google.com/forms/d/e/1FAIpQLScPatNKaZhopNIgK2EjO6WTuy8VueY-RTfu3RUzMyrfsvIDPA/viewform">החידון הקודם שעשיתי</a> לכבוד יום שחרור ירושלים. אפשר גם לשתף את החידון עם אנשים אחרים שיענו לאתגר. אשמח לקבל משוב כאן </p>
+
