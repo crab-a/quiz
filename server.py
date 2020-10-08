33bc6f6013c4d77074a9de75a5b5eae6865857df
crab-a <crab@notmailinator.com>
2020-10-09 00:20:16 +0300

    refactoring images in a3, q9, and q8 fixing bug accessing a3 changing the way we check the answers all is mostly workable(if not presentable) now

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import markdown
 import os
 import csv
-
+import re
 from flask import Flask, render_template, request, redirect
 
 # def md_to_html(md_text):
@@ -16,32 +16,35 @@ from flask import Flask, render_template, request, redirect
 #         with open(f'.\\static\\assets\\texts\\{html_txt_filename}', 'w', encoding='utf-8') as html_file:
 #             html_file.write(html_text)
 quiz = Flask(__name__)
-page_number = 0
-right_answers=['0=so empty', 'בית החולים כפר שאול', 'next answer', 'etc']
+page_number = 1
+answer = ''
 @quiz.route('/')
 def my_home():
     return render_template('intro.html')
 
 
-@quiz.route('/<string:current_page>')
+@quiz.route('/<string:current_page>')  # to be removed in production
 def page_name(current_page=''):
     return render_template(current_page)
 
 
 @quiz.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
-    if request.method == 'POST' and request.form.get('answer') != '':
+    if request.method == 'POST':
         global page_number
         global answers
+        global answer
         try:
-            answer = request.form.get('answer')
+            answer = request.form.to_dict()
+            print(answer)
             answers[f'{page_number}'] = answer
-            if answer == right_answers[page_number]:
+            final_answer = (eval(f'check{page_number}(answer)'))
+            if final_answer:
                 return redirect('/full_answer')
             else:
-                return redirect('/wrong_answer.html')
+                return render_template('/wrong_answer.html')
         except:
-            return 'couldnt get that, try again'
+            raise
     else:
         return redirect('try_again')
 
@@ -58,28 +61,36 @@ def full_answer():
 
 @quiz.route('/nextq')
 def nextq():
-    global  page_number
+    global page_number
     page_number = find_page_number()
-    page_number += 1
-    return render_template(f'Q{page_number}.html')
+    if page_number == 10:
+        return render_template('colophone.html')
+    else:
+        page_number += 1
+        return render_template(f'Q{page_number}.html')
 
 
 @quiz.route('/back')
 def back():
     global page_number
     page_number = find_page_number()
-    page_number -= 1
-    return render_template(f'Q{page_number}.html')
+    if page_number <= 1:
+        return render_template('intro.html')
+    else:
+        page_number -= 1
+        return render_template(f'Q{page_number}.html')
 
 @quiz.route('/submit_quiz', methods=['POST', 'GET'])
 def submit_quiz():
     global answers
+    global answer
     if request.method == 'POST':
         answer = request.form.get('answer')
         answers['feedback'] = answer
         return render_template('summary.html', answers=answers)
 
     else:
+
         return render_template('summary.html', answers=answers)
 
 @quiz.route('/Q0.html')
@@ -93,26 +104,86 @@ def write_to_csv(answer, username):
         csv_writer.writerow([answer])
 
 
-
 def find_page_number():
     page_number = int(request.args.get('page_number'))
-    if page_number == 0:
-        return render_template('intro.html')
-    elif page_number > 10:
-        return render_template('colophone.html')
+    return page_number
+
+def check1(answer):
+    local_answer=answer['answer']
+    if re.search(r'(כפר שאול)', local_answer):
+        return True
     else:
-        return page_number
+        return False
+
+def check2(answer):
+    local_answer = answer['answer']
+    if local_answer == 'ניו יורק':
+        return True
+    else:
+        return False
+
+def check3(answer):
+    local_answer = answer['answer']
+    if local_answer == 'The Wall':
+        return True
+    else:
+        return False
+
+def check4(answer):
+    local_answer = answer['answer']
+    if local_answer == 'Henry':
+        return True
+    else:
+        return False
+
+def check5(answer):
+    local_answer = answer['answer']
+    if local_answer == '5':
+        return True
+    else:
+        return False
+
+def check6(answer):
+    local_answer = answer['answer']
+    if local_answer == 'מסדר הכותל':
+        return True
+    else:
+        return False
+
+def check7(answer):
+    local_answer = answer['answer']
+    if local_answer == 'בשבעת צבעי הקשת בכניסה לגן הילדים ברחוב':
+        return True
+    else:
+        return False
+
+def check8(answer):
+    local_answer = answer['answer']
+    if local_answer == 'Berlin':
+        return True
+    else:
+        return False
 
 
+def check9(answer):
+    try:
+        local_answer = [answer['answer'], answer['answer2'], answer['answer3']]
+        if local_answer == ['Charlemagne', 'Hector', 'King Arthur']:
+            return True
+        else:
+            return False
+    except:
+        return False
+
+def check10(answer):
+    answer = answer['answer']
+    if answer == '10':
+        return True
+    else:
+        return False
 
 answers = {}
 
-# implment all q&a in the html templet of q and a
 # in the htmls change all <p> to <p dir="rtl"> to make rtl
-# get a username
 # export all answers to a csv/google sheets
-# move nav bar to middle
-# get right answers by order into the list and accsess by page num
-    # you can use dict with numbers as keys to get multiple possible answers
-
-#treat edge cases (first qustion back and last qustion next)
+# make the summary page presentable
